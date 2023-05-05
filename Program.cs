ddeefb83e80fafcdbaf94c6195f788d8a5767e90
Samkelo Gigaba <sgigaba@student.wethinkcode.co.za>
2023-05-05 14:07:02 +0200

    Add DevExtreme and Styling

--- Program.cs
+++ Program.cs
@@ -16,6 +16,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
