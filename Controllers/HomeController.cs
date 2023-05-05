666fa8c3433727b7a9b163e2c05c523736eb90d3
Samkelo Gigaba <sgigaba@student.wethinkcode.co.za>
2023-05-05 11:18:00 +0200

    Reverting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,18 +11,5 @@ namespace TestApp.Controllers
         {
             return View();
         }
-
-        public IActionResult Get(DataSourceLoadOptions loadOptions)
-        {
-            var person = new Person()
-            {
-                Id = 1,
-                Name = "Test",
-                Age = 1
-            };
-
-            var people = new List<Person>() { person };
-            return this.Json(DataSourceLoader.Load(people, loadOptions));
-        }
     }
 }
---
+++ WebAPIControllers/HomeWebAPIController.cs
@@ -0,0 +1,23 @@
+﻿using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using TestApp.Models;
+
+namespace TestApp.WebAPIControllers
+{
+    public class HomeWebAPIController : Controller
+    {
+        public IActionResult Get(DataSourceLoadOptions loadOptions)
+        {
+            var person = new Person()
+            {
+                Id = 1,
+                Name = "Test",
+                Age = 1
+            };
+
+            var people = new List<Person>() { person };
+            return this.Json(DataSourceLoader.Load(people, loadOptions));
+        }
+    }
+}
